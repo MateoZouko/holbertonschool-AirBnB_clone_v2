1f140bb5385719e0992d24793717ef9a9c114628
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-11-26 07:16:23 +0000

    fix bug

--- models/user.py
+++ models/user.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+import uuid
+from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from models.base_model import BaseModel, Base
 
+class User(BaseModel, Base):
+    """A class to represent a user."""
+    __tablename__ = 'users'
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a User."""
+        super().__init__(*args, **kwargs)
