2449bf7263857fe7556b93ff61d0d9e008a722c0
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-11-26 07:20:24 +0000

    fix bug

--- models/user.py
+++ models/user.py
@@ -4,16 +4,19 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from models.base_model import BaseModel, Base
+from os import getenv
 
 class User(BaseModel, Base):
     """A class to represent a user."""
     __tablename__ = 'users'
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-
-    def __init__(self, *args, **kwargs):
-        """Initialize a User."""
-        super().__init__(*args, **kwargs)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
