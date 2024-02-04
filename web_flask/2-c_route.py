da69206a987b969efa95e00abcfdd2a2c84a9026
MateoZouko <mateozouko1@gmail.com>
2024-02-04 14:07:45 -0300

    tasks

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,20 +9,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hnbb():
     """Displays 'HBNB'"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Displays 'C' followed by the value of <text>."""
     return f'C {text}'.replace('_', " ")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -9,26 +9,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """Displays 'Hello HBNB!'"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hnbb():
     """Displays 'HBNB'"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Displays 'C' followed by the value of <text>."""
     return f'C {text}'.replace('_', " ")
+
 
 @app.route('/python', strict_slashes=False, defaults={'text': 'is cool'})
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
     """Displays 'Python' followed by the value of <text>"""
     return f'Python {text}'.replace('_', " ")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
